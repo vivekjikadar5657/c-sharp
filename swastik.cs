365f89c613eab0fdefd9ae53b72b02af6f58bf7e
vivekjikadar5657 <vivekjikadara879@gmail.com>
2025-03-09 23:47:35 -0700

    Add files via upload

---
+++ swastik.cs
@@ -0,0 +1,38 @@
+using System;
+namespace VIVEK
+{
+	class Swastik
+	{
+		public static void Main()
+		{
+			Console.WriteLine("Enter Number of * you want = ");
+			int a = Convert.ToInt32(Console.ReadLine());
+			if(a%2==0)
+			{
+				a++;
+				Console.WriteLine("Sorry,But it is even number so we convert it into a odd number for better Results.");
+				Console.WriteLine(a);
+			}
+			double mid = a/2;
+			Console.WriteLine("mid = "+mid);
+			mid++;
+			for(int i = 1; i <= a; i++)
+			{
+				for(int j = 1; j <= a; j++)
+				{
+					if((i==mid||j==mid)||
+						(i==1 && j>=mid && j<=a) || 
+						(i==a && j<=mid && j<=a) ||
+						(j==1 && i>=1 && i<=mid) ||
+						(j==a && i>=mid && i<=a)
+						)
+					{
+						Console.Write("* ");
+					}
+					else
+					{
+						Console.Write("  ");
+					}
+				}
+				Console.WriteLine();
+				}   }   }   }
